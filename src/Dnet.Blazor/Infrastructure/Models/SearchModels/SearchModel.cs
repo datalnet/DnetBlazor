13bc696a8191afc9ff85152e2b0449bb6d6de5f1
Adrian Mustelier <admin@datalnet.com>
2023-01-03 07:53:13 +0100

    Refactor

--- src/Dnet.Blazor/Infrastructure/Models/SearchModels/SearchModel.cs
+++ src/Dnet.Blazor/Infrastructure/Models/SearchModels/SearchModel.cs
@@ -8,7 +8,5 @@ public class SearchModel
 
     public List<AdvancedFilterModel> AdvancedFilterModels { get; set; }
 
-    public List<FilterModel> FilterModels { get; set; }
-
     public SortModel SortModel { get; set; }
 }
