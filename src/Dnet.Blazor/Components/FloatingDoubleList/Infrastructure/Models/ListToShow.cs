a2ae1744c8392b3fac6aad12e8adb9a5234dbbc2
Adrian Mustelier <adrian.mustelier@datalnet.com>
2023-05-30 19:25:33 +0200

    Double list done

---
+++ src/Dnet.Blazor/Components/FloatingDoubleList/Infrastructure/Models/ListToShow.cs
@@ -0,0 +1,7 @@
+﻿namespace Dnet.Blazor.Components.FloatingDoubleList.Infrastructure.Models;
+
+public enum ListToShow
+{
+    Right = 1,
+    Left = 2,
+}
--- src/Dnet.Blazor/Components/FloatingDoubleList/Infrastructure/Models/TransferredItems.cs
+++ src/Dnet.Blazor/Components/FloatingDoubleList/Infrastructure/Models/TransferredItems.cs
@@ -2,9 +2,13 @@
 
 public class TransferredItems<TItem>
 {
+    public List<TItem> RighItems = new();
+
     public List<TItem> RightAddedItems = new();
 
     public List<TItem> RightRemovedItems = new();
+
+    public List<TItem> LeftItems = new();
 
     public List<TItem> LeftAddedItems = new();
 
