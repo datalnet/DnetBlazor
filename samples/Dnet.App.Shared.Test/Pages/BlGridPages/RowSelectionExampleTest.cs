c570155890a3ff56f9572cc9a7c533fd71a4dbad
Adrian Mustelier <adrian.mustelier@datalnet.com>
2023-08-07 13:46:01 +0200

    Release 2.5.9. Grid configuration example added.

--- samples/Dnet.App.Shared.Test/Pages/BlGridPages/RowSelectionExampleTest.cs
+++ samples/Dnet.App.Shared.Test/Pages/BlGridPages/RowSelectionExampleTest.cs
@@ -2,7 +2,6 @@
 using Dnet.App.Shared.Infrastructure.Entities;
 using Dnet.App.Shared.Pages.BlGridPages;
 using Dnet.Blazor.Components.Grid.BlgGrid;
-using Microsoft.Extensions.DependencyInjection;
 using Shouldly;
 
 namespace Dnet.App.Shared.Test.Pages.BlGridPages;
@@ -27,48 +26,20 @@ public class RowSelectionExampleTest
     }
 
     [Fact]
-    public void GridRendersCorrectly()
-    {
-        var cut = RenderComponent();
-
-        var grid = cut.FindComponent<BlgGrid<Person>>();
-
-        //grid.MarkupMatches(GlobalMarkups.BLGRID_MARKUP);
-    }
-
-    [Fact]
     public void GridCreateColumnsCountCorrectly()
     {
         var cut = RenderComponent();
 
-        //ctx.RenderTree.Add<BlgGrid<VesselTypeDto>>(parameters =>
-        //{
-        //    parameters.Add(p => p.GridOptions, cut.Instance.GridOptions);
-
-        //    parameters.Add(p => p.GridColumns, cut.Instance.GridColumns);
-
-        //    parameters.Add(p => p.GroupGridColumn, cut.Instance.GroupGridColumn);
-        //});
-
-        //var grid = ctx.RenderComponent<BlgGrid<Person>>(parameters =>
-        //{
-        //    parameters.Add(p => p.GridOptions, cut.Instance.GridOptions);
-
-        //    parameters.Add(p => p.GridColumns, cut.Instance.GridColumns);
-
-        //    parameters.Add(p => p.GroupGridColumn, cut.Instance.GroupGridColumn);
-        //});
+        cut.Instance.GridOptions.UseVirtualization = false;
 
         var grid = cut.FindComponent<BlgGrid<Person>>();
-
-        var instance = grid.Instance;
 
         var gridCells = grid.FindAll("span.blg-header-cell-text");
 
         var firstCellText = gridCells.First().TextContent;
 
-        gridCells.Count.ShouldBe(2);
+        gridCells.Count.ShouldBe(8);
 
-        firstCellText.ShouldBe("Id");
+        firstCellText.ShouldBe("Name");
     }
 }
--- samples/Dnet.Shared/Pages/BlGridPages/GridConfigurationService.cs
+++ samples/Dnet.Shared/Pages/BlGridPages/GridConfigurationService.cs
@@ -1,73 +1,16 @@
-﻿using Bunit;
-using Dnet.App.Shared.Infrastructure.Entities;
-using Dnet.App.Shared.Infrastructure.Services;
-using Dnet.App.Shared.Pages.BlGridPages;
+﻿using Dnet.App.Shared.Infrastructure.Entities;
 using Dnet.Blazor.Components.Grid.Infrastructure.Entities;
 using Dnet.Blazor.Components.Grid.Infrastructure.Enums;
-using Dnet.Blazor.Components.Grid.Infrastructure.Interfaces;
-using Dnet.Blazor.Components.Grid.Infrastructure.Services;
-using Dnet.Blazor.Components.Overlay.Infrastructure.Interfaces;
-using Dnet.Blazor.Components.Overlay.Infrastructure.Services;
-using Dnet.Blazor.Components.Paginator;
 using Dnet.Blazor.Infrastructure.Models.SearchModels;
-using Microsoft.Extensions.DependencyInjection;
-using Moq;
+using System.Collections.Generic;
 
-namespace Dnet.App.Shared.Test.Pages.BlGridPages;
+namespace Dnet.App.Shared.Pages.BlGridPages;
 
-public class RowSelectionExampleMockedServices
+public class GridConfigurationService : IGridConfigurationService
 {
-    public void RegisterMockedServices(TestContext ctx)
+    public GridOptions<Person> GetGridOptions()
     {
-        var mockIBlGridMessageService = new Mock<IBlGridMessageService<List<RowNode<Person>>>>();
-
-        var mockAdvancedFiltering = new Mock<IAdvancedFiltering<Person>>();
-
-        var mockPaginationService = new Mock<IPaginator>();
-
-        var mockGroupingService = new Mock<IGrouping<Person>>();
-
-        var mockFilteringService = new Mock<IFiltering<Person>>();
-
-        var mockSortingService = new Mock<ISorting<Person>>();
-
-        var mockPersonService = new Mock<IPersonService>();
-
-        var mockBlGridInteropService = new Mock<IBlGridInterop<Person>>();
-
-        List<Person> mockPersons = new() { new Person { Name = "Test", Age = 30 } };
-
-        mockPersonService
-            .Setup(service => service.GetPersons())
-            .ReturnsAsync(mockPersons);
-
-        ctx.Services.AddSingleton(mockPersonService.Object);
-
-        ctx.Services.AddSingleton(mockIBlGridMessageService.Object);
-
-        ctx.Services.AddTransient(typeof(IGrouping<>), typeof(Grouping<>));
-
-        ctx.Services.AddTransient(typeof(ISorting<>), typeof(Sorting<>));
-
-        ctx.Services.AddTransient(typeof(IFiltering<>), typeof(Filtering<>));
-
-        ctx.Services.AddTransient(typeof(IPaginator), typeof(Paginator));
-
-        ctx.Services.AddTransient(typeof(IAdvancedFiltering<>), typeof(AdvancedFiltering<>));
-
-        ctx.Services.AddTransient<DnetOverlayInterop, DnetOverlayInterop>();
-
-        ctx.Services.AddScoped(typeof(IOverlayService), typeof(OverlayService));
-
-        ctx.Services.AddSingleton(mockBlGridInteropService.Object);
-
-        ctx.JSInterop.SetupVoid("blginterop.init", _ => true);
-
-        ctx.JSInterop.SetupVoid("blginterop.dispose", _ => true);
-
-        var gridConfigServiceMock = new Mock<IGridConfigurationService>();
-
-        gridConfigServiceMock.Setup(service => service.GetGridOptions()).Returns(new GridOptions<Person>()
+        return new GridOptions<Person>()
         {
             HeaderHeight = 60,
             RowHeight = 40,
@@ -80,18 +23,18 @@ public class RowSelectionExampleMockedServices
             EnableGrouping = true,
             EnableAdvancedFilter = true,
             GroupDefaultExpanded = false,
-            UseVirtualization = false
-        });
+            UseVirtualization = true
+        };
+    }
 
-        gridConfigServiceMock.Setup(service => service.GetGridColumns()).Returns(() =>
-        {
-            var height = 40;
-            var width = 100;
-            var canGrow = 1;
+    public List<GridColumn<Person>> GetGridColumns()
+    {
+        var height = 40;
+        var width = 100;
+        var canGrow = 1;
 
-            return new List<GridColumn<Person>>
-            {
-                 new GridColumn<Person> {
+        return new List<GridColumn<Person>> {
+             new GridColumn<Person> {
                             ColumnId = 1,
                             ColumnOrder = 1,
                             HeaderName = "Name",
@@ -176,32 +119,13 @@ public class RowSelectionExampleMockedServices
                             CanGrow = canGrow,
                             CellDataFn = (x) => x.RowData.Address
                         }
-                };
-        });
-
-        ctx.Services.AddSingleton(gridConfigServiceMock.Object);
+        };
     }
 }
 
+public interface IGridConfigurationService
+{
+    public GridOptions<Person> GetGridOptions();
 
-// Crear un mock de HttpMessageHandler
-//var mockHandler = new Mock<HttpMessageHandler>();
-
-//mockHandler
-//  .Protected()
-//  .Setup<Task<HttpResponseMessage>>(
-//     "SendAsync",
-//     ItExpr.IsAny<HttpRequestMessage>(),
-//     ItExpr.IsAny<CancellationToken>())
-//  .ReturnsAsync(new HttpResponseMessage()
-//  {
-//      StatusCode = HttpStatusCode.OK,
-//      Content = new StringContent("[{'id':1,'value':'1'}]"),
-//  });
-
-//var httpClient = new HttpClient(mockHandler.Object);
-
-//var mockFactory = new Mock<IHttpClientFactory>();
-//mockFactory.Setup(_ => _.CreateClient(It.IsAny<string>())).Returns(httpClient);
-
-//ctx.Services.AddSingleton(mockFactory.Object);
+    public List<GridColumn<Person>> GetGridColumns();
+}
