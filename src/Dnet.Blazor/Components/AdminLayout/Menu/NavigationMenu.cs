c3e15bd3261d97125b0367b9e54db2fa57d8c6bf
Adrian Mustelier <adrian.mustelier@datalnet.com>
2024-10-16 13:16:31 +0200

    added new way to add icons to menu Items using Renderfragment

--- src/Dnet.Blazor/Components/AdminLayout/Menu/NavigationMenu.cs
+++ src/Dnet.Blazor/Components/AdminLayout/Menu/NavigationMenu.cs
@@ -1,4 +1,6 @@
-﻿namespace Dnet.Blazor.Components.AdminLayout.Menu;
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Dnet.Blazor.Components.AdminLayout.Menu;
 
 public class NavigationMenu
 {
@@ -8,23 +10,25 @@ public class NavigationMenu
 
     public bool IsNode { get; set; }
 
-    public string Text { get; set; }
+    public string Text { get; set; } = string.Empty;
 
-    public string MinifiedText { get; set; }
+	public string MinifiedText { get; set; } = string.Empty;
 
-    public string Link { get; set; }
+    public string Link { get; set; } = string.Empty;
 
-    public string LinkClass { get; set; }
+	public string LinkClass { get; set; } = string.Empty;
 
-    public bool HasImage { get; set; } = false;
+	public bool HasImage { get; set; } = false;
 
-    public string Redirect { get; set; }
+    public string Redirect { get; set; } = string.Empty;
 
-    public string Title { get; set; }
+	public string Title { get; set; } = string.Empty;
 
-    public string IconClass { get; set; }
+	public string IconClass { get; set; } = string.Empty;
 
-    public List<NavigationMenu> Children { get; set; }
+	public List<NavigationMenu>? Children { get; set; }
 
-    public List<string> Rights { get; set; }
+	public List<string> Rights { get; set; } = new();
+
+	public RenderFragment? CustomContent { get; set; }
 }
